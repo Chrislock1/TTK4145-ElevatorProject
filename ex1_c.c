4443fa14ef2715398c3e4959f623f4eb218f13b2
Chrislock1 <fredrikcch@msn.com>
2016-01-17 13:01:02 +0100

    Update ex1_c.c

--- ex1_c.c
+++ ex1_c.c
@@ -4,21 +4,37 @@
 #include <pthread.h>
 #include <stdio.h>
 
+int i = 0;
+
+
 // Note the return type: void*
-void* someThreadFunction(){
-    printf("Hello from a thread!\n");
-    return NULL;
+void* threadAdder(){
+    for(int x = 0; x< 1000000; ++x){
+	++i;
+  }
+}
+
+void* threadSubtractor(){
+    for(int y = 0; y< 1000000; ++y){
+	--i;
+	
+  }
 }
 
 
 
 int main(){
-    pthread_t someThread;
-    pthread_create(&someThread, NULL, someThreadFunction, NULL);
-    // Arguments to a thread would be passed here ---------^
+    pthread_t adder;
+    pthread_t subtractor;
+
+    pthread_create(&adder, NULL, threadAdder, NULL);
+    pthread_create(&subtractor, NULL, threadSubtractor, NULL);
     
-    pthread_join(someThread, NULL);
-    printf("Hello from main!\n");
+    pthread_join(adder, NULL);
+    pthread_join(subtractor, NULL);
+
+    printf("i = %i \n", i);
+
     return 0;
     
 }
