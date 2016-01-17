63bb29e314ad31f74718af33deffd7f3fab73646
Chrislock1 <fredrikcch@msn.com>
2016-01-17 13:18:23 +0100

    Create ex1_py.py

---
+++ ex1_py.py
@@ -0,0 +1,34 @@
+
+# Python 3.3.3 and 2.7.6
+# python helloworld_python.py
+
+from threading import Thread
+
+i = 0
+def threadAdder():
+    for x in range(1000000):
+	i += 1
+
+def threadSubtractor():
+    for x in range(1000000):
+	i -= 1
+# Potentially useful thing:
+#   In Python you "import" a global variable, instead of "export"ing it when you declare it
+#   (This is probably an effort to make you feel bad about typing the word "global")
+
+
+
+def main():
+    adder = Thread(target = threadAdder, args = (),)
+    subtractor = Thread(target = threadSubtractor, args = (),)
+
+    adder.start()
+    subtractor.start()
+
+    adder.join()
+    subtractor.join()
+
+    print(i)
+
+
+main()
