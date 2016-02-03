581c238c6d43bbb633581d8bd09bcac965a27fde
Chrislock1 <fredrikcch@msn.com>
2016-02-03 12:15:38 +0100

    Create defines.h

---
+++ defines.h
@@ -0,0 +1,22 @@
+//
+// Created by student on 03.02.16.
+//
+
+#ifndef EX3_1_CPP_DEFINES_H
+#define EX3_1_CPP_DEFINES_H
+
+//General definitions
+#define TRUE 1
+#define FALSE 0
+
+#define NULL 0
+
+//Network Values
+#define BROADCAST_IP ((const char*) "129.241.187.255")
+
+
+#define NUM_FLOORS 4
+
+
+
+#endif //EX3_1_CPP_DEFINES_H
